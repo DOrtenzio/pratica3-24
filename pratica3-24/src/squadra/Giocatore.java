879ade06b18e1a334af9ce6d3c97463c17bf2b5b
DOrtenzio <gutroch@gmail.com>
2024-10-07 15:27:42 +0200

    Classe giocatore set get attributi e costruttori

---
+++ pratica3-24/src/squadra/Giocatore.java
@@ -0,0 +1,59 @@
+package squadra;
+
+public class Giocatore {
+    private String nome;
+    private boolean capitano;
+    private int goal;
+
+    //Metodi get e set
+    public int getGoal() { return goal; }
+    public String getNome() { return nome; }
+    public boolean isCapitano() { return capitano; }
+
+    public void setCapitano(boolean capitano) { this.capitano = capitano; }
+    public void setGoal(int goal) { this.goal = goal; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    //Costruttori
+    public Giocatore(){
+        nome=null;
+        capitano=false;
+        goal=0;
+    }
+    public Giocatore(String nome){
+        this.nome=nome;
+        capitano=false;
+        goal=0;
+    }
+    public Giocatore(boolean capitano){
+        nome=null;
+        this.capitano=capitano;
+        goal=0;
+    }
+    public Giocatore(int goal){
+        nome=null;
+        capitano=false;
+        this.goal=goal;
+    }
+    public Giocatore(String nome, boolean capitano){
+        this.nome=nome;
+        this.capitano=capitano;
+        goal=0;
+    }
+    public Giocatore(String nome, int goal){
+        this.nome=nome;
+        capitano=false;
+        this.goal=goal;
+    }
+    public Giocatore(boolean capitano, int goal){
+        nome=null;
+        this.capitano=capitano;
+        this.goal=goal;
+    }
+    public Giocatore(String nome,boolean capitano, int goal){
+        this.nome=nome;
+        this.capitano=capitano;
+        this.goal=goal;
+    }
+}
+
